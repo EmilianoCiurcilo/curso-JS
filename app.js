8896981a537aa7a1c67d5a345c41d22dd89461f0
EmilianoCiurcilo <ciurcilo03@gmail.com>
2024-07-21 19:54:21 -0300

    2da entrega

--- app.js
+++ app.js
@@ -1,27 +1,32 @@
-function convertir (){
-    let dolarOficial = 900
-    let dolarBlue = 1300
-
-    
-    let cambio = prompt ('Bienvenido, a que tipo de dolar desea convertir? "oficial" - "blue"')
-
-    while (cambio != "blue" && cambio != "oficial"){
-        alert("ingrese correctamente oficial o blue")
-        cambio = prompt ('Bienvenido, a que tipo de dolar desea convertir? "oficial" - "blue"')
-    };
-    
-    if(cambio == "oficial"){
-        let monto = prompt('ingrese el monto en dolares a cambiar')
-        let conversion = `${monto*dolarOficial}`
-        alert (`La conversion es $${conversion} pesos`)
+const dolares = [
+    {
+    dolar: "oficial",
+    valor: 900
+    },
+    {
+    dolar: "blue",
+    valor: 1300
     }
-    
-    if(cambio == "blue"){
-        let monto = prompt('ingrese el monto en dolares a cambiar')
-        let conversion =`${monto*dolarBlue}`
-        alert (`La conversion es $${conversion} pesos`)
+]
+class Dolar {
+    constructor(dolar, valor) {
+      (this.dolar = dolar), (this.valor = valor);
     }
+  }
+
+const agregarDolares = () => {
+    let dolar = prompt("ingrese el nombre del dolar");
+    let valor = Number(prompt("ingrese el valor de cambio"));
+    alert(`Agregaste el dolar ${dolar} a $${valor} correctamente`)
     
-    alert ("¡Gracias por su visita!")
+    let dolarNuevo = new Dolar (dolar, valor);
+  
+    dolares.push(dolarNuevo);
+  };
+
+let continuar = confirm("quieres agregar un nuevo dolar?");
+while (continuar) {
+  agregarDolares();
+  continuar = confirm("quieres agregar otro?");
 }
-convertir()
+
